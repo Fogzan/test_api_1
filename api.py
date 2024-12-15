1075e5b56da65cf35644ecfcf806999b0b22ce48
Fogzan <fogzan@yandex.ru>
2024-12-16 01:39:58 +0300

    commit

--- api.py
+++ api.py
@@ -13,7 +13,7 @@ app.add_middleware(
 )
 
 price = {
-    "BTC": 10000,
+    "BTC": 100000,
     "ETH": 10000,
 }
 
@@ -27,11 +27,11 @@ def index(fsym, tsyms):
         return {"Error":"Ты говорил, что запросы только с USD"}
     if (not (fsym in price)):
         random_value = round(random.uniform(0.5, 1.5), 3)
-        return {f"{fsym}":random_value}
+        return {"USD":random_value}
     if (fsym in price):
         random_par = round(random.uniform(0.9, 1.1), 3)
         price[fsym] = (round(price[fsym] * random_par * 100)) / 100
-        return {f"{fsym}":price[fsym]}
+        return {"USD":price[fsym]}
     return {"Error":"ошибка"}
 
 
